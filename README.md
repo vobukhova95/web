03019578b61ec93c859aa4ac11d6a3f08a0d98d0
Veronika Obukhova <vera.obukhova.95@gmail.com>
2025-06-10 00:02:21 +0700

    Change tests

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/u75a9ih03su8i2x8?svg=true)](https://ci.appveyor.com/project/vobukhova95/web)
--- src/test/java/AppOrderTest.java
+++ src/test/java/AppOrderTest.java
@@ -40,12 +40,65 @@ public class AppOrderTest {
     void shouldSendForm() {
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Иванов");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79135432321");
-        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("label[data-test-id='agreement']")).click();
         driver.findElement(By.cssSelector("button")).click();
         WebElement result = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
         assertTrue(result.isDisplayed());
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", result.getText().trim());
     }
 
+    @Test
+    void shouldReturnErrorForNameIfEmpty() {
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+78972345443");
+        driver.findElement(By.cssSelector("label[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        WebElement result = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub"));
+        assertTrue(result.isDisplayed());
+        assertEquals("Поле обязательно для заполнения", result.getText().trim());
+    }
+
+    @Test
+    void shouldReturnErrorForPhoneIfEmpty() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Иванов");
+        driver.findElement(By.cssSelector("label[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        WebElement result = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub"));
+        assertTrue(result.isDisplayed());
+        assertEquals("Поле обязательно для заполнения", result.getText().trim());
+    }
+
+    @Test
+    void shouldReturnErrorForLabelIfEmpty() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Иванов");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79135432321");
+        driver.findElement(By.cssSelector("button")).click();
+        WebElement result = driver.findElement(By.cssSelector(".checkbox__text"));
+        assertEquals("rgba(255, 92, 92, 1)", result.getCssValue("color"));
+    }
+
+    @Test
+    void shouldReturnErrorForName() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ivan Markov");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+78972345443");
+        driver.findElement(By.cssSelector("label[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        WebElement result = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub"));
+        assertTrue(result.isDisplayed());
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", result.getText().trim());
+    }
+
+    @Test
+    void shouldReturnErrorForPhone() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Вера Климова");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+789723454435");
+        driver.findElement(By.cssSelector("label[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        WebElement result = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub"));
+        assertTrue(result.isDisplayed());
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", result.getText().trim());
+    }
+
 
 }
+
+
